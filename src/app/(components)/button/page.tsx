8dcf6398f14106594c7355aa1c70503215293ced
Gustavo Sales <gus@codegus.com.br>
2024-01-20 16:37:03 -0300

    feat: button component

--- src/app/(components)/button/page.tsx
+++ src/app/(components)/button/page.tsx
@@ -1,7 +1,14 @@
-export default function Button() {
+import {Button, buttonVariants} from "@/components/ui/button";
+import Link from "next/link";
+
+export default function ButtonPage() {
   return (
-    <>
-      <h2>Button Component</h2>
-    </>
+    <div className="flex gap-2">
+      <Button>Clique Aqui</Button>
+
+      <Link href="/logout" className={buttonVariants({variant: "destructive"})}>
+        Logout
+      </Link>
+    </div>
   );
 }
