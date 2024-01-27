a0172376df69a2cb77c1c74037cb9fe9d64365f6
Gustavo Sales <gus@codegus.com.br>
2024-01-27 10:05:56 -0300

    add dark, light and themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import Link from "next/link";
+import ThemeSwitcher from "@/core-components/theme-switcher";
+import ThemeProvider from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "Série Shadcn/UI | Codegus",
@@ -14,26 +16,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="h-screen flex flex-col gap-24 items-center justify-center">
-        <header className="flex flex-col gap-4 text-center">
-          <h1 className="text-3xl font-bold">
-            <span>Codegus</span>{" "}
-            <span className="text-zinc-500">{"// "}Shadcn UI</span>
-          </h1>
-          <div className="font-light text-zinc-400">
-            Série de Vídeos passando por cada componente...
-          </div>
-          <nav className="flex gap-1 justify-center">
-            <Link
-              href="/button"
-              className="hover:underline font-bold text-blue-500"
-            >
-              Button
-            </Link>
-          </nav>
-        </header>
-        <main className="border border-solid border-zinc-200 rounded py-14 px-36">
-          {children}
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="flex flex-col gap-4 text-center">
+            <h1 className="text-3xl font-bold">
+              <span>Codegus</span>{" "}
+              <span className="text-zinc-500">{"// "}Shadcn UI</span>
+            </h1>
+            <div className="font-light text-zinc-400">
+              Série de Vídeos passando por cada componente...
+            </div>
+            <ThemeSwitcher />
+            <nav className="flex gap-1 justify-center">
+              <Link
+                href="/button"
+                className="hover:underline font-bold text-blue-500"
+              >
+                Button
+              </Link>
+            </nav>
+          </header>
+          <main className="border border-solid border-zinc-200 rounded py-14 px-36">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
